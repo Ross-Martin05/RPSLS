3747591daec555f2a3a2735c128ae5723a342eba
Ross-Martin5 <rmartin.ross5@gmail.com>
2022-03-04 01:25:31 -0600

    updated RPSLS File

---
+++ rpsls/algorithm.py
@@ -0,0 +1,84 @@
+""" 
+Step 1: Rules
+    
+    Play a game using choices; Rock, paper, scissors, lizard, or spock.
+   
+    Each user/computer choice can win or lose depending on the head to head matchup.
+    
+    Listed below are the senarios in which each choice can win or lose a game.
+       
+        Rock crushes Scissors
+        Scissors cuts Paper
+        Paper covers Rock 
+        Rock crushes Lizard 
+        Lizard poisons Spock 
+        Spock vaporizes Scissors 
+        Scissors decapitates Lizard 
+        Lizard eats Paper
+        Paper disproves Spock 
+        Spock vaporizes Rock
+
+Step 2: Users
+     Select if the user would like to play against another user or the computer
+
+
+Step 3: Begin game
+    Ask user to select a playor object using; R,P,S,L,SP.
+
+Step 4: Computer selection
+    If computer is playing, have computer make a random selection.
+
+Step 5: Run game
+    Display users and/or computer choices
+
+Step 6: Display Winner
+
+Step 7: Show Score
+
+Step 8: prompt next round
+    if ther is no current victor, repeat steps 3-8 until user or computer has won 2 games.
+
+Step 9: once the user or computer has won 2 games, display the victor.
+
+Step 10: Play again or End game
+    choose to play another match or exit game. """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
